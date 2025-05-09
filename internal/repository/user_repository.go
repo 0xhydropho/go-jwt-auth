caf428c839f34fec25505a5ed165d58d0d3d3f26
irvan pramana <darknet1255@gmail.com>
2025-05-09 13:25:21 +0700

    add user repository interface and implementation

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"github.com/0xirvan/go-jwt-auth/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+	FindByID(id uint) (*models.User, error)
+	FindByEmail(email string) (*models.User, error)
+}
+
+type UserRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &UserRepositoryImpl{
+		DB: db,
+	}
+}
+
+func (r *UserRepositoryImpl) Create(user *models.User) error {
+	return r.DB.Create(user).Error
+}
+
+func (r *UserRepositoryImpl) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.DB.Where("id = ?", id).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepositoryImpl) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
